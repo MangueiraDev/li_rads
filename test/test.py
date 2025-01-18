98200d6107fa2c8a4bb087fe96c09127d3d3780a
e-Mangueira <nm.mangueira@ufma.br>
2025-01-18 10:02:40 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,6 +1,3 @@
 from tensorflow.keras.models import load_model
 import numpy as np
 
-model = load_model('unet_model.h5')
-image = np.expand_dims(normalized_image, axis=0)
-segmented_image = model.predict(image)[0]
